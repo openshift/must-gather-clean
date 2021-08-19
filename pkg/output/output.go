30c9ee5065c93dda2eec29708639984fc5544c22
Thomas Jungblut <tjungblu@redhat.com>
2021-08-19 13:43:26 +0200

    add overwrite flag for the output folder

--- pkg/output/output.go
+++ pkg/output/output.go
@@ -48,29 +48,46 @@ func (f *fsWriter) Writer(parentPath, name string, permissions os.FileMode) (Clo
 	}, writer, nil
 }
 
-// NewFSWriter returns an Outputter which writes the files and directories to a specified location.
-func NewFSWriter(path string) (Outputter, error) {
+func EnsureOutputPath(path string, deleteIfExists bool) error {
 	info, err := os.Stat(path)
 	if err != nil {
 		if !os.IsNotExist(err) {
-			return nil, err
+			return err
 		} else {
 			err := os.Mkdir(path, 0700)
 			if err != nil {
-				return nil, fmt.Errorf("failed to create output directory %s: %w", path, err)
+				return fmt.Errorf("failed to create output directory '%s': %w", path, err)
 			}
 		}
 	} else {
-		if !info.IsDir() {
-			return nil, fmt.Errorf("output destination must be a directory: %s", path)
-		}
-		entries, err := os.ReadDir(path)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get contents of output directory %s: %w", path, err)
-		}
-		if len(entries) != 0 {
-			return nil, fmt.Errorf("output directory %s is not empty", path)
+		if deleteIfExists {
+			err = os.RemoveAll(path)
+			if err != nil {
+				return fmt.Errorf("error while deleting the output path '%s': %w", path, err)
+			}
+
+			err = os.Mkdir(path, 0700)
+			if err != nil {
+				return fmt.Errorf("failed to create output directory '%s': %w", path, err)
+			}
+		} else {
+			if !info.IsDir() {
+				return fmt.Errorf("output destination must be a directory: '%s'", path)
+			}
+			entries, err := os.ReadDir(path)
+			if err != nil {
+				return fmt.Errorf("failed to get contents of output directory '%s': %w", path, err)
+			}
+			if len(entries) != 0 {
+				return fmt.Errorf("output directory %s is not empty", path)
+			}
 		}
 	}
+
+	return nil
+}
+
+// NewFSWriter returns an Outputter which writes the files and directories to a specified location.
+func NewFSWriter(path string) (Outputter, error) {
 	return &fsWriter{outputDir: path}, nil
 }
