825f5ff0179843de596b75dd51bff9dfa12f52e0
Arjun Naik <arjun.rn@gmail.com>
2021-08-20 09:50:34 +0200

    Only obfuscated user supplied domains
    This change adds a new field in the obfuscation type of Domain which
    is used to obfuscated only domains and subdomains of the input.

--- pkg/obfuscator/domain_test.go
+++ pkg/obfuscator/domain_test.go
@@ -3,106 +3,85 @@ package obfuscator
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDomainObfuscatorContents(t *testing.T) {
 	for _, tc := range []struct {
-		name   string
-		tlds   []string
-		input  []string
-		output []string
-		report map[string]string
+		name    string
+		domains []string
+		input   []string
+		output  []string
+		report  map[string]string
 	}{
 		{
-			name: "basic",
-			tlds: []string{".com", ".info", ".org"},
+			name:    "basic",
+			domains: []string{"redhat.com", "openshift.com", "okd.io"},
 			input: []string{
-				"received request on test.com",
-				"received request on https://test.com",
+				"received request on openshift.com",
+				"received request on https://docs.okd.io",
 			},
 			output: []string{
-				"received request on obfuscated0001.ext",
-				"received request on https://obfuscated0001.ext",
+				"received request on domain0000001",
+				"received request on https://docs.domain0000002",
 			},
-			report: map[string]string{"test.com": "obfuscated0001.ext"},
+			report: map[string]string{
+				"openshift.com": "domain0000001",
+				"docs.okd.io":   "docs.domain0000002",
+			},
 		},
 		{
-			name: "subdomain",
-			tlds: []string{".com", ".info", ".org"},
+			name: "subdomains",
+			domains: []string{
+				"docs.okd.io",
+				"cloud.redhat.com",
+			},
+			input: []string{
+				"okd.io",
+				"docs.okd.io",
+				"cloud.redhat.com",
+				"beta.cloud.redhat.com",
+			},
+			output: []string{
+				"okd.io",
+				"domain0000001",
+				"domain0000002",
+				"beta.domain0000002",
+			},
+			report: map[string]string{
+				"docs.okd.io":           "domain0000001",
+				"cloud.redhat.com":      "domain0000002",
+				"beta.cloud.redhat.com": "beta.domain0000002",
+			},
+		},
+		{
+			name:    "multi-level subdomains",
+			domains: []string{"test.com", "test.info", "test.org"},
 			input: []string{
 				"received request on abc.test.com",
-				"received request on def.test.com",
+				"received request on def.test.info",
+				"received request on ghi.abc.test.com",
+				"received request on pqr.ghi.abc.test.com",
 			},
 			output: []string{
-				"received request on abc.obfuscated0001.ext",
-				"received request on def.obfuscated0001.ext",
+				"received request on abc.domain0000001",
+				"received request on def.domain0000002",
+				"received request on ghi.abc.domain0000001",
+				"received request on pqr.ghi.abc.domain0000001",
 			},
 			report: map[string]string{
-				"abc.test.com": "abc.obfuscated0001.ext",
-				"def.test.com": "def.obfuscated0001.ext",
-			},
-		},
-		{
-			name: "multiple domains",
-			tlds: []string{".com", ".info", ".org"},
-			input: []string{
-				"request to test1.com failed",
-				"request to test2.com succeeded",
-			},
-			output: []string{
-				"request to obfuscated0001.ext failed",
-				"request to obfuscated0002.ext succeeded",
-			},
-			report: map[string]string{
-				"test1.com": "obfuscated0001.ext",
-				"test2.com": "obfuscated0002.ext",
-			},
-		},
-		{
-			name: "non tld host",
-			tlds: []string{".com", ".info", ".org"},
-			input: []string{
-				"request to test1.remotehost failed",
-				"request to test2.remotehost succeeded",
-			},
-			output: []string{
-				"request to test1.obfuscated0001 failed",
-				"request to test2.obfuscated0001 succeeded",
-			},
-			report: map[string]string{
-				"test1.remotehost": "test1.obfuscated0001",
-				"test2.remotehost": "test2.obfuscated0001",
-			},
-		},
-		{
-			name: "ignore ip addresses",
-			input: []string{
-				"calling 192.168.1.10 for backlog",
-				"calling http://192.168.1.10 for backlog",
-			},
-			output: []string{
-				"calling 192.168.1.10 for backlog",
-				"calling http://192.168.1.10 for backlog",
-			},
-			report: map[string]string{},
-		},
-		{
-			name: "ip address and host",
-			tlds: []string{".com"},
-			input: []string{
-				"received request for https://service.example.com from 192.168.1.20",
-			},
-			output: []string{
-				"received request for https://service.obfuscated0001.ext from 192.168.1.20",
-			},
-			report: map[string]string{
-				"service.example.com": "service.obfuscated0001.ext",
+				"abc.test.com":         "abc.domain0000001",
+				"def.test.info":        "def.domain0000002",
+				"ghi.abc.test.com":     "ghi.abc.domain0000001",
+				"pqr.ghi.abc.test.com": "pqr.ghi.abc.domain0000001",
 			},
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			o := NewDomainObfuscator(tc.tlds)
+			o, err := NewDomainObfuscator(tc.domains)
+			require.NoError(t, err)
 			for idx, i := range tc.input {
 				output := o.Contents(i)
 				assert.Equal(t, tc.output[idx], output)
@@ -114,37 +93,41 @@ func TestDomainObfuscatorContents(t *testing.T) {
 
 func TestDomainObfuscator_FileName(t *testing.T) {
 	for _, tc := range []struct {
-		name   string
-		input  string
-		output string
-		report map[string]string
+		name    string
+		input   string
+		output  string
+		domains []string
+		report  map[string]string
 	}{
 		{
-			name:   "domain with extension",
-			input:  "test.host1.log",
-			output: "test.obfuscated0001.log",
+			name:    "domain with extension",
+			domains: []string{"test.com"},
+			input:   "requests.test.com.log",
+			output:  "requests.domain0000001.log",
 			report: map[string]string{
-				"test.host1": "test.obfuscated0001",
+				"requests.test.com": "requests.domain0000001",
 			},
 		},
 		{
-			name:   "no domain with extension",
-			input:  "node1.log",
-			output: "node1.log",
-			report: map[string]string{},
-		},
-		{
-			name:   "no domain and no extension",
-			input:  "report",
-			output: "report",
-			report: map[string]string{},
+			name:    "non-matching domain",
+			domains: []string{"test.com"},
+			input:   "report.test",
+			output:  "report.test",
+			report:  map[string]string{},
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			o := NewDomainObfuscator(nil)
+			o, err := NewDomainObfuscator(tc.domains)
+			require.NoError(t, err)
 			output := o.FileName(tc.input)
 			assert.Equal(t, tc.output, output)
 			assert.Equal(t, tc.report, o.ReportingResult())
 		})
 	}
+}
+
+func TestBadDomainInput(t *testing.T) {
+	_, err := NewDomainObfuscator([]string{"[mustgather.com"})
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "failed to generate regex")
 }
