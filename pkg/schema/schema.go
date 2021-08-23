1b533fdb8f4ceb56ee7dbbeb174f2bb2c5cfd5f1
Thomas Jungblut <tjungblu@redhat.com>
2021-08-23 12:22:27 +0200

    update schema to reflect the implementation

--- pkg/schema/schema.go
+++ pkg/schema/schema.go
@@ -6,6 +6,79 @@ import "fmt"
 import "reflect"
 import "encoding/json"
 
+type Obfuscate struct {
+	// The list of domains and their subdomains which should be obfuscated in the
+	// output, only used with the type Domain obfuscator.
+	Domains []string `json:"domains,omitempty"`
+
+	// when replacementType 'Regex' is used, the supplied Golang regexp
+	// (https://pkg.go.dev/regexp) will be used to detect the string that should be
+	// replaced. The regex is line based, spanning multi-line regex statements is not
+	// supported.
+	Regex *string `json:"regex,omitempty"`
+
+	// on replacement 'Keywords', this will override a given input string with another
+	// output string. On duplicate keys it will use the last defined value as
+	// replacement. The input values are matched in a case-sensitive fashion and only
+	// as a full words, substrings must be matched using a regex.
+	Replacement ObfuscateReplacement `json:"replacement,omitempty"`
+
+	// This defines how the detected string will be replaced. Type 'Consistent' will
+	// guarantee the same input will always create the same output string. 'Static' is
+	// used by default and will just try to mask the matching input.
+	ReplacementType ObfuscateReplacementType `json:"replacementType,omitempty"`
+
+	// This determines if the obfuscation should be performed on the file name or on
+	// the file contents. The file contents are obfuscated by default.
+	Target ObfuscateTarget `json:"target,omitempty"`
+
+	// type defines the kind of detection you want to use. For example IP will find IP
+	// addresses, whereas Keywords will find keywords defined in the 'replacement'
+	// mapping. Domain must be used in conjunction with the 'domains' property, that
+	// defines what domains should be obfuscated. MAC currently only supports static
+	// replacement where a detected mac address will be replaced by 'x'. Regex should
+	// be used with the 'regex' property that will define the regex, here the
+	// replacement also will be static by 'x'-ing out the matched string.
+	Type ObfuscateType `json:"type"`
+}
+
+// on replacement 'Keywords', this will override a given input string with another
+// output string. On duplicate keys it will use the last defined value as
+// replacement. The input values are matched in a case-sensitive fashion and only
+// as a full words, substrings must be matched using a regex.
+type ObfuscateReplacement map[string]string
+
+type ObfuscateReplacementType string
+
+const ObfuscateReplacementTypeConsistent ObfuscateReplacementType = "Consistent"
+const ObfuscateReplacementTypeStatic ObfuscateReplacementType = "Static"
+
+type ObfuscateTarget string
+
+const ObfuscateTargetAll ObfuscateTarget = "All"
+const ObfuscateTargetFileContents ObfuscateTarget = "FileContents"
+const ObfuscateTargetFileName ObfuscateTarget = "FileName"
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (j *ObfuscateTarget) UnmarshalJSON(b []byte) error {
+	var v string
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	var ok bool
+	for _, expected := range enumValues_ObfuscateTarget {
+		if reflect.DeepEqual(v, expected) {
+			ok = true
+			break
+		}
+	}
+	if !ok {
+		return fmt.Errorf("invalid value (expected one of %#v): %#v", enumValues_ObfuscateTarget, v)
+	}
+	*j = ObfuscateTarget(v)
+	return nil
+}
+
 // UnmarshalJSON implements json.Unmarshaler.
 func (j *Omit) UnmarshalJSON(b []byte) error {
 	var raw map[string]interface{}
@@ -21,44 +94,6 @@ func (j *Omit) UnmarshalJSON(b []byte) error {
 		return err
 	}
 	*j = Omit(plain)
-	return nil
-}
-
-// UnmarshalJSON implements json.Unmarshaler.
-func (j *ObfuscateType) UnmarshalJSON(b []byte) error {
-	var v string
-	if err := json.Unmarshal(b, &v); err != nil {
-		return err
-	}
-	var ok bool
-	for _, expected := range enumValues_ObfuscateType {
-		if reflect.DeepEqual(v, expected) {
-			ok = true
-			break
-		}
-	}
-	if !ok {
-		return fmt.Errorf("invalid value (expected one of %#v): %#v", enumValues_ObfuscateType, v)
-	}
-	*j = ObfuscateType(v)
-	return nil
-}
-
-// UnmarshalJSON implements json.Unmarshaler.
-func (j *SchemaJsonConfig) UnmarshalJSON(b []byte) error {
-	var raw map[string]interface{}
-	if err := json.Unmarshal(b, &raw); err != nil {
-		return err
-	}
-	type Plain SchemaJsonConfig
-	var plain Plain
-	if err := json.Unmarshal(b, &plain); err != nil {
-		return err
-	}
-	if len(plain.Obfuscate) < 1 {
-		return fmt.Errorf("field %s length: must be >= %d", "obfuscate", 1)
-	}
-	*j = SchemaJsonConfig(plain)
 	return nil
 }
 
@@ -83,6 +118,24 @@ func (j *ObfuscateReplacementType) UnmarshalJSON(b []byte) error {
 }
 
 // UnmarshalJSON implements json.Unmarshaler.
+func (j *SchemaJsonConfig) UnmarshalJSON(b []byte) error {
+	var raw map[string]interface{}
+	if err := json.Unmarshal(b, &raw); err != nil {
+		return err
+	}
+	type Plain SchemaJsonConfig
+	var plain Plain
+	if err := json.Unmarshal(b, &plain); err != nil {
+		return err
+	}
+	if len(plain.Obfuscate) < 1 {
+		return fmt.Errorf("field %s length: must be >= %d", "obfuscate", 1)
+	}
+	*j = SchemaJsonConfig(plain)
+	return nil
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
 func (j *OmitType) UnmarshalJSON(b []byte) error {
 	var v string
 	if err := json.Unmarshal(b, &v); err != nil {
@@ -103,83 +156,29 @@ func (j *OmitType) UnmarshalJSON(b []byte) error {
 }
 
 // UnmarshalJSON implements json.Unmarshaler.
-func (j *ObfuscateTarget) UnmarshalJSON(b []byte) error {
+func (j *ObfuscateType) UnmarshalJSON(b []byte) error {
 	var v string
 	if err := json.Unmarshal(b, &v); err != nil {
 		return err
 	}
 	var ok bool
-	for _, expected := range enumValues_ObfuscateTarget {
+	for _, expected := range enumValues_ObfuscateType {
 		if reflect.DeepEqual(v, expected) {
 			ok = true
 			break
 		}
 	}
 	if !ok {
-		return fmt.Errorf("invalid value (expected one of %#v): %#v", enumValues_ObfuscateTarget, v)
+		return fmt.Errorf("invalid value (expected one of %#v): %#v", enumValues_ObfuscateType, v)
 	}
-	*j = ObfuscateTarget(v)
+	*j = ObfuscateType(v)
 	return nil
 }
-
-type Obfuscate struct {
-	// The list of domains and their subdomains which should be obfuscated in the
-	// output.
-	Domains []string `json:"domains,omitempty"`
-
-	// when replacementType=regex is used, the supplied  regex (Golang regexp
-	// https://pkg.go.dev/regexp) will be used to detect the string that should be
-	// replaced. The regex is line based, spanning multi-line regex statements is not
-	// supported.
-	Regex *string `json:"regex,omitempty"`
-
-	// on any replacement type, this will override a given input string with another
-	// output string. On duplicate keys it will use the last defined value as
-	// replacement. The input values are matched in a case-sensitive fashion and only
-	// as a full words, substrings must be matched using a regex. With type regex, the
-	// keys will be considered as capture group values, if there is no mapping
-	// available it will be replaced with a random string of the same size.
-	Replacement ObfuscateReplacement `json:"replacement,omitempty"`
-
-	// This defines how the detected string will be replaced. Type 'Consistent' will
-	// guarantee the same input will always create the same output string and 'Random'
-	// will just create a random replacement string of the same length as the input.
-	// 'Static' relies solely on the replacement object to define an input/output
-	// mapping.
-	ReplacementType ObfuscateReplacementType `json:"replacementType"`
-
-	// This determines if the obfuscation should be performed on the file name or on
-	// the file contents. The file contents are obfuscated by default
-	Target ObfuscateTarget `json:"target,omitempty"`
-
-	// type defines the kind of detection you want to use. For example IP will find IP
-	// addresses, whereas Keywords will find predefined keywords.
-	Type ObfuscateType `json:"type"`
-}
-
-// on any replacement type, this will override a given input string with another
-// output string. On duplicate keys it will use the last defined value as
-// replacement. The input values are matched in a case-sensitive fashion and only
-// as a full words, substrings must be matched using a regex. With type regex, the
-// keys will be considered as capture group values, if there is no mapping
-// available it will be replaced with a random string of the same size.
-type ObfuscateReplacement map[string]string
-
-type ObfuscateReplacementType string
-
-const ObfuscateReplacementTypeConsistent ObfuscateReplacementType = "Consistent"
-const ObfuscateReplacementTypeRandom ObfuscateReplacementType = "Random"
-const ObfuscateReplacementTypeStatic ObfuscateReplacementType = "Static"
-
-type ObfuscateTarget string
-
-const ObfuscateTargetAll ObfuscateTarget = "All"
-const ObfuscateTargetFileContents ObfuscateTarget = "FileContents"
-const ObfuscateTargetFileName ObfuscateTarget = "FileName"
 
 type ObfuscateType string
 
 const ObfuscateTypeDomain ObfuscateType = "Domain"
+const ObfuscateTypeIP ObfuscateType = "IP"
 
 // UnmarshalJSON implements json.Unmarshaler.
 func (j *Obfuscate) UnmarshalJSON(b []byte) error {
@@ -196,7 +195,7 @@ func (j *Obfuscate) UnmarshalJSON(b []byte) error {
 		return err
 	}
 	if v, ok := raw["replacementType"]; !ok || v == nil {
-		plain.ReplacementType = "Random"
+		plain.ReplacementType = "Static"
 	}
 	if v, ok := raw["target"]; !ok || v == nil {
 		plain.Target = "FileContents"
@@ -205,7 +204,6 @@ func (j *Obfuscate) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-const ObfuscateTypeIP ObfuscateType = "IP"
 const ObfuscateTypeKeywords ObfuscateType = "Keywords"
 const ObfuscateTypeMAC ObfuscateType = "MAC"
 const ObfuscateTypeRegex ObfuscateType = "Regex"
@@ -242,17 +240,18 @@ type OmitType string
 const OmitTypeFile OmitType = "File"
 const OmitTypeKubernetes OmitType = "Kubernetes"
 
-// This configuration defines the behaviour of the must-gather-clean CLI.
+// This configuration defines the behaviour of the must-gather-clean CLI. The CLI
+// helps to obfuscate and omit output from OpenShift debug information
+// ('must-gathers'). You can find more information in our GitHub repository at
+// https://github.com/openshift/must-gather-clean.
 type SchemaJson struct {
-	// The config schema defines the behaviour of the must-gather-clean CLI. There are
-	// two sections, "omit" which defines the omission behaviour and "obfuscate" which
-	// defines the obfuscation behaviour.
+	// There are two main sections, "omit" which defines the omission behaviour and
+	// "obfuscate" which defines the obfuscation behaviour.
 	Config SchemaJsonConfig `json:"config"`
 }
 
-// The config schema defines the behaviour of the must-gather-clean CLI. There are
-// two sections, "omit" which defines the omission behaviour and "obfuscate" which
-// defines the obfuscation behaviour.
+// There are two main sections, "omit" which defines the omission behaviour and
+// "obfuscate" which defines the obfuscation behaviour.
 type SchemaJsonConfig struct {
 	// The obfuscation schema determines what is being detected and how it is being
 	// replaced. We ship with several built-in replacements for common types such as
@@ -274,7 +273,6 @@ type SchemaJsonConfig struct {
 
 var enumValues_ObfuscateReplacementType = []interface{}{
 	"Consistent",
-	"Random",
 	"Static",
 }
 var enumValues_ObfuscateTarget = []interface{}{
