3e11504914a7a735eb6883bd1f1eb0291113caf0
Thomas Jungblut <tjungblu@redhat.com>
2021-09-03 15:47:51 +0200

    add more cli tests

--- pkg/cli/cli_test.go
+++ pkg/cli/cli_test.go
@@ -7,6 +7,9 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/openshift/must-gather-clean/pkg/kube"
+	"github.com/openshift/must-gather-clean/pkg/schema"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -79,4 +82,106 @@ func TestEnsureOutputPathDeletesIfRequired(t *testing.T) {
 	_, err = os.Stat(toBeDeletedFile)
 	require.Error(t, err)
 	require.Truef(t, os.IsNotExist(err), "file %s exists even though it shouldn't", toBeDeletedFile)
+}
+
+func TestRunFailsOnNegativeAndZeroWorkers(t *testing.T) {
+	err := Run("", "", "", false, "", 0)
+	assert.Equal(t, err, fmt.Errorf("invalid number of workers specified %d", 0))
+	err = Run("", "", "", false, "", -2)
+	assert.Equal(t, err, fmt.Errorf("invalid number of workers specified %d", -2))
+}
+
+func TestFailConfigReading(t *testing.T) {
+	testDir, err := os.MkdirTemp(os.TempDir(), "test-dir-*")
+	require.NoError(t, err)
+	defer func() {
+		_ = os.RemoveAll(testDir)
+	}()
+
+	err = Run("some.yaml", "", testDir, false, "", 1)
+	assert.ErrorIs(t, err, os.ErrNotExist)
+}
+
+func TestCreateObfuscatorFromFullConfig(t *testing.T) {
+	sampleRegex := "^would-match$"
+	config := &schema.SchemaJson{Config: schema.SchemaJsonConfig{
+		Obfuscate: []schema.Obfuscate{
+			{
+				Type: schema.ObfuscateTypeKeywords,
+				Replacement: map[string]string{
+					"something": "something else",
+				},
+				Target: schema.ObfuscateTargetFileContents,
+			},
+			{
+				Type:            schema.ObfuscateTypeMAC,
+				ReplacementType: schema.ObfuscateReplacementTypeStatic,
+				Target:          schema.ObfuscateTargetFileContents,
+			},
+			{
+				Type:   schema.ObfuscateTypeRegex,
+				Regex:  &sampleRegex,
+				Target: schema.ObfuscateTargetFileContents,
+			},
+			{
+				Type:            schema.ObfuscateTypeDomain,
+				Domains:         []string{"something.com"},
+				Target:          schema.ObfuscateTargetFileContents,
+				ReplacementType: schema.ObfuscateReplacementTypeStatic,
+			},
+			{
+				Type:            schema.ObfuscateTypeIP,
+				ReplacementType: schema.ObfuscateReplacementTypeStatic,
+				Target:          schema.ObfuscateTargetFileContents,
+			},
+		},
+		Omit: nil,
+	}}
+
+	mfo, err := createObfuscatorsFromConfig(config)
+	require.NoError(t, err)
+	assert.Equal(t, "something else", mfo.Contents("something"))
+}
+
+func TestCreateOmitter(t *testing.T) {
+	sampleApiVersion := "v1"
+	sampleKind := "Resource"
+	sampleRegex := "would-match"
+
+	config := &schema.SchemaJson{Config: schema.SchemaJsonConfig{
+		Omit: []schema.Omit{
+			{
+				Type: schema.OmitTypeKubernetes,
+				KubernetesResource: &schema.OmitKubernetesResource{
+					ApiVersion: &sampleApiVersion,
+					Kind:       &sampleKind,
+					Namespaces: []string{"kube-system"},
+				}},
+			{
+				Type:    schema.OmitTypeFile,
+				Pattern: &sampleRegex,
+			},
+		},
+	}}
+
+	om, err := createOmittersFromConfig(config)
+	require.NoError(t, err)
+
+	match, err := om.OmitPath("would-match")
+	require.NoError(t, err)
+	assert.Truef(t, match, "'would-match' should match the path omission config")
+	match, err = om.OmitPath("would-not-match")
+	require.NoError(t, err)
+	assert.Falsef(t, match, "'would-not-match' should match the path omission config")
+
+	match, err = om.OmitKubeResource(&kube.ResourceListWithPath{
+		ResourceList: kube.ResourceList{
+			Items: []kube.Resource{
+				{ApiVersion: sampleApiVersion, Kind: sampleKind, Metadata: kube.Metadata{Namespace: "kube-system"}},
+			},
+		},
+		Path: "some-path",
+	})
+	require.NoError(t, err)
+	assert.Truef(t, match, "k8s resource with the exact same input should match")
 }
