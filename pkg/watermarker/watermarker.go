9edb182e4d4ba199ca4a86d44d7dc43264e85821
Sherine Khoury <skhoury@redhat.com>
2021-09-17 09:06:14 +0200

    Add watermark at the end of the run WaterMarker interface has 2 implementations : EmptyWaterMarker, SimpleWaterMarker

---
+++ pkg/watermarker/watermarker.go
@@ -0,0 +1,44 @@
+package watermarking
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	version "github.com/openshift/must-gather-clean/pkg/version"
+)
+
+type WaterMarker interface {
+	// WriteWaterMarkFile creates a watermark file in the specified path
+	WriteWaterMarkFile(path string) error
+}
+
+type EmptyWaterMarker struct{}
+
+type SimpleWaterMarker struct{}
+
+func NewEmptyWaterMarker() *EmptyWaterMarker {
+	return &EmptyWaterMarker{}
+}
+
+func (e *EmptyWaterMarker) WriteWaterMarkFile(path string) error {
+	_, err := os.Create(filepath.Join(path, "watermark.txt"))
+	if err != nil {
+		return fmt.Errorf("failed to create watermark file in output folder: %w", err)
+	}
+	return nil
+}
+
+func NewSimpleWaterMarker() *SimpleWaterMarker {
+	return &SimpleWaterMarker{}
+}
+
+func (s *SimpleWaterMarker) WriteWaterMarkFile(path string) error {
+	contents := time.Now().UTC().String() + " - " + version.GetVersion().Version
+	err := os.WriteFile(filepath.Join(path, "watermark.txt"), []byte(contents), 0644)
+	if err != nil {
+		return fmt.Errorf("failed to create watermark file in output folder: %w", err)
+	}
+	return nil
+}
---
+++ pkg/watermarker/watermarker_test.go
@@ -0,0 +1,41 @@
+package watermarking
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	version "github.com/openshift/must-gather-clean/pkg/version"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEmptyWaterMarkingHappyPath(t *testing.T) {
+	w := NewEmptyWaterMarker()
+
+	tmpInputDir, err := os.MkdirTemp(os.TempDir(), "watermarker-*")
+	require.NoError(t, err)
+	defer func() {
+		_ = os.RemoveAll(tmpInputDir)
+	}()
+
+	err = w.WriteWaterMarkFile(tmpInputDir)
+	require.NoError(t, err)
+	require.FileExists(t, filepath.Join(tmpInputDir, "watermark.txt"))
+}
+
+func TestSimpleWaterMarkingHappyPath(t *testing.T) {
+	w := NewSimpleWaterMarker()
+
+	tmpInputDir, err := os.MkdirTemp(os.TempDir(), "watermarker-*")
+	require.NoError(t, err)
+	defer func() {
+		_ = os.RemoveAll(tmpInputDir)
+	}()
+
+	err = w.WriteWaterMarkFile(tmpInputDir)
+	require.NoError(t, err)
+	require.FileExists(t, filepath.Join(tmpInputDir, "watermark.txt"))
+	data, err := os.ReadFile(filepath.Join(tmpInputDir, "watermark.txt"))
+	require.NoError(t, err)
+	require.Contains(t, string(data), version.GetVersion().Version)
+}
